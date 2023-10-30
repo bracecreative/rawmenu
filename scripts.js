d1adf8200af46dbdb16de495093da9e1878706c7
Todd Owen Mpeli <toddowenmpeli@rocktmail.com>
2023-10-30 14:59:44 +0000

    Created new floder strutures for the javascript, css files and other php components files. Secondly, Create the dog food calulator javascript file and imported the file into the script.js file. Thirdly, created woocommerce specific css styling files, including base colours and global variable css files. Alternatively, imported these files into the main styles.css.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,26 @@
-let new_note_button = document.querySelector('.new-private-note');
-if( new_note_button ) {
-    new_note_button.addEventListener('click', function(e) {
-        e.preventDefault();
+import "./js/dog-food-calculator";
 
-        var private_notes_wrapper = document.getElementById('private-notes');
-        let private_notes_count = private_notes_wrapper.querySelectorAll('textarea').length;
-            private_notes_count++;
+let new_note_button = document.querySelector(".new-private-note");
 
-        let new_private_field = document.createElement("textarea");
-            new_private_field.setAttribute('rows', '2');
-            new_private_field.setAttribute('cols', '30');
-            new_private_field.setAttribute('name', 'wc_private_notes_'+private_notes_count);
-            new_note_button.parentElement.insertBefore(new_private_field, new_note_button);
-    });
+if (new_note_button) {
+	new_note_button.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		var private_notes_wrapper = document.getElementById("private-notes");
+		let private_notes_count =
+			private_notes_wrapper.querySelectorAll("textarea").length;
+		private_notes_count++;
+
+		let new_private_field = document.createElement("textarea");
+		new_private_field.setAttribute("rows", "2");
+		new_private_field.setAttribute("cols", "30");
+		new_private_field.setAttribute(
+			"name",
+			"wc_private_notes_" + private_notes_count
+		);
+		new_note_button.parentElement.insertBefore(
+			new_private_field,
+			new_note_button
+		);
+	});
 }
